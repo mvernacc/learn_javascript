b209bd21c3d95b56ef9e1a5cbdbbdae7a7a0c9f2
Matthew Vernacchia <mvernacc@mit.edu>
2021-03-05 11:24:05 -0500

    Finish scope and closure lesson.

--- javascript_info_tutorial/06_functions/603_scope_closure/scope_closure.js
+++ javascript_info_tutorial/06_functions/603_scope_closure/scope_closure.js
@@ -112,3 +112,31 @@
     console.log(JSON.stringify(
         users.sort(byField('age')), null, 2));
 }
+
+// Army of functions
+{
+    function makeArmy() {
+        let shooters = [];
+      
+        let i = 0;
+        // Could also use a for loop, whcih provides a new Lexical Environment for each iteration, unlike while
+        while (i < 10) {
+            let my_number = i; // Create a new number in the iteration's Lexical Environment
+            let shooter = function() { // create a shooter function,
+                console.log(`shooter ${my_number}`); // that should show its number
+            };
+            shooters.push(shooter); // and add it to the array
+            i++;
+        }
+      
+        // ...and return the array of shooters
+        return shooters;
+    }
+    
+    let army = makeArmy();
+    
+    // all shooters show 10 instead of their numbers 0, 1, 2, 3...
+    army[0](); // 10 from the shooter number 0
+    army[1](); // 10 from the shooter number 1
+    army[2](); // 10 ...and so on.
+}
