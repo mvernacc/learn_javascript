f74166f455d55572d696cac4be08de1abfd7dbf1
Matthew Vernacchia <mvernacc@mit.edu>
2021-03-26 17:33:29 -0400

    Start Web Audio getting data from microphone.

--- ts_spectrogram/src/index.ts
+++ ts_spectrogram/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world')
+function createAudioContext(stream: MediaStream) {
+    let audioContext = new AudioContext();
+    let source = audioContext.createMediaStreamSource(stream);
+    let analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+
+    let bufferLength = analyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+
+    setInterval(
+        () => {
+            analyser.getByteFrequencyData(dataArray);
+            console.log(dataArray);
+        },
+        30
+    );
+}
+
+let constraints = {audio: true};
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => createAudioContext(stream));
