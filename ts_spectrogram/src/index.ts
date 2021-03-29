b8ce98589b1415a67dfd110d148104eed039a49e
Matthew Vernacchia <mvernacc@mit.edu>
2021-03-29 16:28:53 -0400

    Draft of time-history spectrogram using circular buffer from CBuffer. Need to figure out how to make the module import of CBuffer work.

--- ts_spectrogram/src/index.ts
+++ ts_spectrogram/src/index.ts
@@ -1,23 +1,29 @@
+// import CBuffer = require('CBuffer');
+import CBuffer from 'CBuffer';
+
 function createAudioContext(stream: MediaStream) {
     let audioContext = new AudioContext();
     let source = audioContext.createMediaStreamSource(stream);
     let analyser = audioContext.createAnalyser();
     source.connect(analyser);
 
-    let bufferLength = analyser.frequencyBinCount;
-    let dataArray = new Uint8Array(bufferLength);
+    const numTimesteps = 20;
+    let audioFreqPowerHistory = new CBuffer<Uint8Array>(numTimesteps);
+    for (let i = 0; i < numTimesteps; i++) {
+        audioFreqPowerHistory.push(new Uint8Array(analyser.frequencyBinCount));
+    }
 
     setInterval(
         () => {
-            analyser.getByteFrequencyData(dataArray);
-            console.log(dataArray);
-            drawSpectogram(dataArray);
+            audioFreqPowerHistory.rotateRight();
+            analyser.getByteFrequencyData(audioFreqPowerHistory.first()!);
+            drawSpectogram(audioFreqPowerHistory);
         },
         30
     );
 }
 
-function drawSpectogram(dataArray: Uint8Array) {
+function drawSpectogram(audioFreqPowerHistory: CBuffer<Uint8Array>) {
     let canvasCtx = (<HTMLCanvasElement>document.getElementById('spectrogram-canvas')).getContext('2d');
     if (canvasCtx === null) return;
 
@@ -25,12 +31,20 @@ function drawSpectogram(dataArray: Uint8Array) {
     let canvasHeight = 500;
 
     canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
-    let dy = canvasHeight / dataArray.length;
+    const timeNumBins = audioFreqPowerHistory.length;
+    const freqNumBins = audioFreqPowerHistory.first()!.length;
+    const dx = canvasWidth / timeNumBins;
+    const dy = canvasHeight / freqNumBins;
+    let x = 0;
     let y = 0;
-    for (let i = 0; i < dataArray.length; i++) {
-        canvasCtx.fillStyle = `rgb(${dataArray[i]},0,0)`;
-        canvasCtx.fillRect(0, y, 20, dy);
-        y +=dy;
+    for (let timeIndex = 0; timeIndex < timeNumBins; timeIndex++) {
+        const audioFreqPower = audioFreqPowerHistory.get(timeIndex)!;
+        for (let freqIndex = 0; freqIndex < freqNumBins; freqIndex++) {
+            canvasCtx.fillStyle = `rgb(${audioFreqPower[freqIndex]},0,0)`;
+            canvasCtx.fillRect(x, y, dx, dy);
+            y += dy;
+        }
+        x += dx;
     }
 }
 
