91acdd7ffdb1b5ff7c3a758f35c3ef0edd963ece
Matthew Vernacchia <mvernacc@mit.edu>
2021-03-30 16:22:48 -0400

    snowpack config.

--- ts_spectrogram/snowpack.config.js
+++ ts_spectrogram/snowpack.config.js
@@ -1,3 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-    plugins: ['@snowpack/plugin-typescript'],
-  };
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    '@snowpack/plugin-typescript',
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: "chrome",
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
