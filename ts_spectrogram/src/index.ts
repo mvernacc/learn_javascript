50ade93b2d5484db0d46c79fc27bd4d04613b052
Matthew Vernacchia <mvernacc@mit.edu>
2021-03-31 15:17:59 -0400

    Add time axis.

--- ts_spectrogram/src/index.ts
+++ ts_spectrogram/src/index.ts
@@ -9,8 +9,12 @@ function createAudioContext(stream: MediaStream) {
     source.connect(analyser);
 
     const numTimesteps = 20;
+    const timeStep = 0.030;  // seconds
+    let timeStamps = new CBuffer<Date>(numTimesteps);
     let audioFreqPowerHistory = new CBuffer<Uint8Array>(numTimesteps);
+    const now = new Date();
     for (let i = 0; i < numTimesteps; i++) {
+        timeStamps.push(new Date(now.getTime() - i * 1000 * timeStep));
         audioFreqPowerHistory.push(new Uint8Array(analyser.frequencyBinCount));
     }
 
@@ -19,22 +23,23 @@ function createAudioContext(stream: MediaStream) {
 
     setInterval(
         () => {
+            timeStamps.unshift(new Date());
             audioFreqPowerHistory.rotateRight();
             analyser.getByteFrequencyData(audioFreqPowerHistory.first()!);
-            drawSpectogram(canvas, audioFreqPowerHistory);
+            drawSpectogram(canvas, audioFreqPowerHistory, timeStamps);
         },
-        30
+        1000 * timeStep
     );
 }
 
 const yAxisWidth = 50; // pixels
-const xAxisHeight = 50; // pixels
+const xAxisHeight = 80; // pixels
 
 function drawYAxis(canvas: HTMLCanvasElement, sampleRate: number, frequencyBinCount: number) {
     let canvasCtx = canvas.getContext('2d')!;
     const plotHeight = canvas.height - xAxisHeight;
 
-    // Label
+    // Axis label
     canvasCtx.save();
     canvasCtx.translate(10, canvas.height / 2);
     canvasCtx.rotate(-Math.PI/2);
@@ -42,7 +47,7 @@ function drawYAxis(canvas: HTMLCanvasElement, sampleRate: number, frequencyBinCo
     canvasCtx.fillText("Frequency [kHz]", 0, 0);
     canvasCtx.restore();
 
-    // Ticks
+    // Ticks and tick labels
     const nyquistFreq = sampleRate / 2; // hertz
     const pixelPerHertz = plotHeight / nyquistFreq;
     const tickSpacingHertz = 2000;
@@ -63,12 +68,25 @@ function drawYAxis(canvas: HTMLCanvasElement, sampleRate: number, frequencyBinCo
     }
 }
 
-function drawSpectogram(canvas: HTMLCanvasElement, audioFreqPowerHistory: CBuffer<Uint8Array>) {
+/**
+ * From https://stackoverflow.com/a/10073788
+ */
+function padLeading(n: number, width: number, z?: string) {
+    z = z || '0';
+    const nStr = n.toString();
+    return nStr.length >= width ? nStr : new Array(width - nStr.length + 1).join(z) + nStr;
+  }
+
+function drawSpectogram(canvas: HTMLCanvasElement,
+                        audioFreqPowerHistory: CBuffer<Uint8Array>,
+                        timeStamps: CBuffer<Date>) {
     let canvasCtx = canvas.getContext('2d')!;
     const plotWidth = canvas.width - yAxisWidth;
     const plotHeight = canvas.height - xAxisHeight;
 
-    canvasCtx.clearRect(yAxisWidth, 0, plotWidth, canvas.height);
+
+    // Update the spectrogram itself.
+    canvasCtx.clearRect(yAxisWidth, 0, plotWidth, plotHeight);
     const timeNumBins = audioFreqPowerHistory.length;
     const freqNumBins = audioFreqPowerHistory.first()!.length;
     const dx = plotWidth / timeNumBins;
@@ -84,6 +102,32 @@ function drawSpectogram(canvas: HTMLCanvasElement, audioFreqPowerHistory: CBuffe
             y -= dy;
         }
         x -= dx;
+    }
+
+    // Update the time axis.
+    canvasCtx.clearRect(0, plotHeight, canvas.width, xAxisHeight);
+    // Axis label
+    canvasCtx.textAlign = "center";
+    canvasCtx.fillText("Time [min:sec]", canvas.width / 2, canvas.height - 10);
+    // Ticks and tick labels
+    const tickLength = 10;
+    for (let timeIndex = 0; timeIndex < timeNumBins; timeIndex++) {
+        x = canvas.width - (dx / 2) - (timeIndex * dx);
+        canvasCtx.save();
+        canvasCtx.translate(x, plotHeight);
+        // Draw ticks
+        canvasCtx.moveTo(0, 0);
+        canvasCtx.lineTo(0, tickLength);
+        canvasCtx.stroke();
+        // Draw tick labels
+        canvasCtx.textAlign = "right";
+        canvasCtx.rotate(-Math.PI / 4);
+        const t = timeStamps.get(timeIndex)!;
+        canvasCtx.fillText(
+            `${padLeading(t.getMinutes(), 2)}:${padLeading(t.getSeconds(), 2)}.${padLeading(t.getMilliseconds(), 3)}`,
+            -5, tickLength + 10
+        );
+        canvasCtx.restore();
     }
 }
 
